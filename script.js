220811bab1c55dba0a5a0a8a7d7276bfd247832a
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-10-29 10:42:53 +0200

    Text effect fades in on scroll on mobile and tablet devices

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ navMenu.forEach((element) => {
         this.classList.add('active')
     })
 })
+
+window.addEventListener('scroll', reveal)
+
+function reveal() {
+    let reveals = document.querySelectorAll('.reveal')
+
+    for (var i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight
+        let revealTop = reveals[i].getBoundingClientRect().top
+        let revealPoint = 150
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('activeScroll')
+        } else {
+            reveals[i].classList.remove('activeScroll')
+        }
+    }
+}
